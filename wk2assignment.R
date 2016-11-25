1936ce9526b184fc2cfd443923a8fab1257120cc
ugjim2k <ugjim2k@gmail.com>
2016-11-25 19:32:58 +0300

    Updated the function to clean data. Now to merge data sets - the SummmaryData with the original data set

--- wk2assignment.R
+++ wk2assignment.R
@@ -19,62 +19,74 @@ extractedFile <- 'activity.csv'
 
 # Download and load packages
 checkPackages <- function() {
-  missingPackages <- packages[(!(packages %in% installed.packages()))]
-  if(length(missingPackages) > 0)
-    install.packages(missingPackages)
-  
-  if(!all(unlist(lapply(packages, require, character.only = TRUE))))
-    stop('Cannot install packages. Quitting...')
+    missingPackages <- packages[(!(packages %in% installed.packages()))]
+    if(length(missingPackages) > 0)
+        install.packages(missingPackages)
+    
+    if(!all(unlist(lapply(packages, require, character.only = TRUE))))
+        stop('Cannot install packages. Quitting...')
 }
 
 # Download and load dataset
 loadData <- function() {
-  fullFileName <- file.path(dataFolder, filename)
-  
-  dir.create(dataFolder, showWarnings = FALSE)
-  download.file(dataURL, fullFileName)
-  unzip(zipfile = fullFileName, exdir = dataFolder, files = extractedFile)
-  
-  # Return dataset
-  tbl_df(read.csv(file.path(dataFolder, extractedFile)))
+    fullFileName <- file.path(dataFolder, filename)
+    
+    dir.create(dataFolder, showWarnings = FALSE)
+    #  download.file(dataURL, fullFileName)
+    unzip(zipfile = fullFileName, exdir = dataFolder, files = extractedFile)
+    
+    # Return dataset
+    tbl_df(read.csv(file.path(dataFolder, extractedFile)))
 }
 
-cleanData <- function(data_file) {
-  data_file$date <- ymd(data_file$date)
-  data_file
+cleanData <- function(data_file, valueOfNASteps = 0) {
+    # Clean up the dates usling lubridate and replace NA in steps field
+    # with value in valueOfNASteps argument
+    
+    data_file$date <- ymd(data_file$date)
+    data_file$steps <- replace(
+        dataFile$steps, which(is.na(dataFile$steps)), 
+        valueOfNASteps)
+    
+    data_file
 }
 
 # Executing all functions for 'testing'
 main <- function() {
-  # Load dependencies
-  checkPackages()
-  
-  # Loading and preprocessing the data
-  dataFile <- loadData()
-  dataFile <- cleanData(dataFile)
-  
-  # What is mean  number of steps taken per day?
-  totalsteps <- dataFile %>% group_by(date) %>% 
-    summarise(total = sum(steps, na.rm = TRUE))
-  
-  totalsteps %>% ggplot(aes(total)) + geom_histogram(binwidth = 350) + 
-    ggtitle('Histogram of total number of steps')
-  
-  summaryData <- dataFile %>% group_by(date) %>% 
-    summarise(total = sum(steps, na.rm = TRUE), 
-              mean = mean(steps, na.rm = TRUE), 
-              median = median(steps, na.rm = TRUE))
-  
-  # What is the average daily activity pattern?
-  # (Will need to add text to mention NaN values are causing gaps in line)
-  ggplot(summaryData, aes(date, mean)) + 
-      geom_line() + 
-      labs(title = 'Time Series Plot', x = 'Date', y = 'Average Step Count')
-  
-  (summaryData %>% filter(mean == max(mean, na.rm = TRUE))) $ date
-  
-  ## Imputing missing values
-  NATotals <- sum(is.na(dataFile)) # Total number of NAs
-  
-  
- }
+    # Load dependencies
+    checkPackages()
+    
+    # Loading the original data set into the dataSet variable
+    dataSet <- loadData()
+    
+    # Cleaning steps in original data set by replacing NA with 0 (default)
+    dataFile <- cleanData(dataSet)
+    
+    # What is mean  number of steps taken per day?
+    # -------------------------------------------------
+    totalsteps <- dataFile %>% group_by(date) %>% 
+        summarise(total = sum(steps, na.rm = TRUE))
+    
+    totalsteps %>% ggplot(aes(total)) + geom_histogram(binwidth = 350) + 
+        ggtitle('Histogram of total number of steps')
+    
+    summaryData <- dataFile %>% group_by(date) %>% 
+        summarise(total = sum(steps, na.rm = TRUE), 
+                  mean = mean(steps, na.rm = TRUE), 
+                  median = median(steps, na.rm = TRUE))
+    
+    # What is the average daily activity pattern?
+    # ------------------------------------------------
+    ggplot(summaryData, aes(date, mean)) + 
+        geom_line() + 
+        labs(title = 'Time Series Plot', x = 'Date', y = 'Average Step Count')
+    
+    (summaryData %>% filter(mean == max(mean, na.rm = TRUE))) $ date
+    
+    ## Imputing missing values
+    ## -----------------------------------
+    NATotals <- sum(is.na(dataFile)) # Total number of NAs
+    
+    #Let's merge the summaryData object with the dataFile object to create one.
+    merge(dataFile, summaryData, by = 'date') %>% head(500)
+}
